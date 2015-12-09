db5b1a736e8a7f9f1aeb9be7d781e35085dc9322
ozhurakovskaya <oxana.zhurakovskaya@fh-bielefeld.de>
2015-12-09 07:46:45 +0100

    some simple partial implementation of RefCounter and SmartPointer

--- smartpointer/src/RefCounter.cpp
+++ smartpointer/src/RefCounter.cpp
@@ -11,26 +11,26 @@
  * ===========================================================================
  */
 
-RefCounter(){
+RefCounter::RefCounter(){
+	RefCounter::n =0;
+}
+void RefCounter::inc(){
+	RefCounter::n++;
+}
+void RefCounter::dec(){
+	RefCounter::n--;
+}
+bool RefCounter::isZero() const{
+	return n< 1;
+}
+unsigned int RefCounter::getRefCount() const{
+	return RefCounter::n;
+}
+RefCounter::RefCounter(const RefCounter::RefCounter&) = delete{
 
 }
-void inc(){
-
-}
-void dec(){
-
-}
-bool isZero() const{
-
-}
-unsigned int getRefCount() const{
-
-}
-RefCounter(const RefCounter&) = delete{
-
-}
-RefCounter &operator=(const RefCounter&) = delete{
-
+RefCounter::RefCounter RefCounter::&operator=(const RefCounter::RefCounter& rc) = delete{
+	RefCounter::n = rc.getRefCount();
 }
 
 
--- smartpointer/src/SmartPointer.h
+++ smartpointer/src/SmartPointer.h
@@ -66,43 +66,56 @@ private:
  * ===========================================================================
  */
 
-SmartPointer(T * const p = nullptr){
+SmartPointer::SmartPointer(T * const p = nullptr){
+	pObj = p;
+	rc = new RefCounter;
+	if(p != nullptr){
+		rc.inc();
+	}
 
 }
-SmartPointer(const SmartPointer&){
+SmartPointer::SmartPointer(const SmartPointer::SmartPointer& sp){
+	SmartPointer::pObj = sp.getObject();
+	SmartPointer::rc = sp.getRefCounter();
+   SmartPointer::rc.inc();
+}
+virtual SmartPointer::~SmartPointer(){
+	if(SmartPointer::rc.isZero()){
+		//löschen inhalt und pointer selbst
+		SmartPointer::deleteObject();
+	} else {
+		//löschen nur pointer und rc.dec()
+
+	}
+}
+T SmartPointer::*operator->() const{
 
 }
-virtual ~SmartPointer(){
+T SmartPointer::&operator*() const{
 
 }
-T *operator->() const{
+const T SmartPointer::*getObject() const{
 
 }
-T &operator*() const{
+const RefCounter SmartPointer::*getRefCounter() const{
 
 }
-const T *getObject() const{
+const SmartPointer SmartPointer::&operator=(T * const p){
 
 }
-const RefCounter *getRefCounter() const{
+const SmartPointer SmartPointer::&operator=(const SmartPointer&){
 
 }
-const SmartPointer &operator=(T * const p){
-
+bool SmartPointer::operator==(const SmartPointer& sp) const{
+	return (SmartPointer::*pObj==sp.getObject()) && (SmartPointer::rc == sp.getRefCounter());
 }
-const SmartPointer &operator=(const SmartPointer&){
-
-}
-bool operator==(const SmartPointer&) const{
-
-}
-bool operator!=(const SmartPointer&) const{
-
+bool SmartPointer::operator!=(const SmartPointer&) const{
+	return !((SmartPointer::*pObj==sp.getObject()) && (SmartPointer::rc == sp.getRefCounter())) ;
 }
 operator bool() const{
 
 }
-void deleteObject(){
+void SmartPointer::deleteObject(){
 
 }
 #endif  // SMARTPOINTER_H
